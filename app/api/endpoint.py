a04fd58300be6860bdb26f9b3958ed8384daebe4
andrewho <andyminhtuanho@gmail.com>
2022-07-03 17:25:33 -0700

    Added GET /player-count endpoint

--- app/api/endpoint.py
+++ app/api/endpoint.py
@@ -12,7 +12,7 @@ def test():
     return "hello world"
 
 
-@api.get("/get-owned-games")
+@api.get("/owned-games")
 def get_owned_games():
     steam_id = request.args.get("steam_id")
     if not steam_id:
@@ -22,3 +22,22 @@ def get_owned_games():
         f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={STEAM_API_KEY}&steamid={steam_id}&include_appinfo=true&include_played_free_games=true&format=json")
 
     return response.json()
+
+
+@api.get("/user")
+def get_user_info():
+    pass
+
+
+@api.get("/player-count")
+def get_player_count():
+    app_id = request.args.get("app_id")
+
+    if not app_id:
+        return bad_request("app_id must be supplied")
+
+    url = f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1?appid={app_id}"
+    response = requests.get(url)
+    data = response.json()
+
+    return data
